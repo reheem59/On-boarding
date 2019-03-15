71708536ada0f8669ea7c5f2a79b578d0897befc
reheem59 <you@example.comreheem.butt@gmail.com>
2019-03-16 03:23:43 +0800

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
+
+
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -64,20 +68,122 @@
             }
         </style>
     </head>
+    
+    
     <body>
+    
+    <div class="container">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                    <li><a  data-toggle="modal" data-target="#myModal">Login</a></li>
+                    
+                              <!-- Modal -->
+                                <div class="modal fade" id="myModal" role="dialog" >
+                                <div class="modal-dialog"style="padding-left: 50%;padding-top: 3% ">
+                                 <!-- Modal content-->
+                                    <div class="modal-content  modal-sm   ">
+                                        <div class="modal-header ">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 class="modal-title">Login</h4>
+                                        </div>
+                                        <div class="modal-body " >
+                                        <div class="input-group">
+
+                                           <!-- <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                            <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                                            </div>
+                                            <div class="input-group">
+                                            <br>
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                            <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                                            </div>
+                                            <br>
+                                            <button type="button" class="btn btn-primary">Login</button>
+                                            <br>
+                                            <a href="#">Forgot Password?</a>
+                                            <hr>
+                                            
+                                            <p>New User?</p>
+                                            <a href="{{ route('register') }}" type="button" class="btn btn-primary">Sign up</a>
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>-->
+                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-3 control-label">E-Mail Address</label>
+
+                        <div class="col-md-6">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-3 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 ">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+
+                                <a class="btn btn-link col-md-9" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                                            
+                        <p>New User?</p>
+                        <a href="{{ route('register') }}" type="button" class="btn btn-primary">Sign up</a>
+                    </form>
+                                </div>
+                                </div>
+                                
+                        
                     @endauth
                 </div>
             @endif
 
-            <div class="content">
+            
+        </div>
+        </div>
+        </div>
+        <div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
@@ -93,3 +199,4 @@
         </div>
     </body>
 </html>
+
